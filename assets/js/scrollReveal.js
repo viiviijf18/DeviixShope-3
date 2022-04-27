c61b2b4505250e8cce11b28516638e009ba65510
viiviijf18 <vivianajf18@gmail.com>
2022-04-27 13:16:27 -0500

    version1

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#imagenes',{delay: 200});
+ScrollReveal().reveal('#video',{delay: 200});
+ScrollReveal().reveal('#adoptar',{delay: 200});
+ScrollReveal().reveal('.navbar',{delay: 200});
